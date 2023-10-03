a771188f55c344c48dd9a82f4a3bab23c55e0399
wilsonsaa <wilsonsaaaldana@gmail.com>
2023-10-03 00:13:09 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,43 @@
+function encriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let textoCifrado = texto
+        .replace(/e/gi, "enter")
+        .replace(/i/gi,"imes")
+        .replace(/a/gi, "ai")
+        .replace(/o/gi, "ober")
+        .replace(/u/gi, "ufat");
+    
+    if(texto.length !=0){
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto Encriptado con éxito";
+        parrafo.textContent = "";
+    }else{
+        tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+        parrafo.textContent="Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes Ingresar alún texto ");
+    }
+}
+
+function desencriptar(){
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let textoCifrado = texto
+        .replace(/enter/gi, "e")
+        .replace(/imes/gi,"i")
+        .replace(/ai/gi, "a")
+        .replace(/ober/gi, "o")
+        .replace(/ufat/gi, "u");
+
+        if(texto.length !=0){
+            document.getElementById("texto").value = textoCifrado;
+            tituloMensaje.textContent = "Texto Desencriptado con éxito";
+            parrafo.textContent = "";
+        }else{
+            tituloMensaje.textContent = "Ningun mensaje fue encontrado";
+            parrafo.textContent="Ingresa el texto que deseas encriptar o desencriptar";
+            alert("Debes Ingresar alún texto ");
+        }
+}
